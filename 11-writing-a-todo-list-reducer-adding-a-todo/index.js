522466bbe635445434451162d4600fa75e40f6bb
Zhongyuan <yawenzhongyuan@gmail.com>
2017-12-19 16:07:45 +0800

    Add 11,12 writing-a-todo-list-reducer

---
+++ 11-writing-a-todo-list-reducer-adding-a-todo/index.js
@@ -0,0 +1,37 @@
+// TODO
+const todos = (state = [], action) => {
+    switch (action.type) {
+    case 'ADD_TODO':
+        return [
+            ...state,
+            {
+                id: action.id,
+                text: action.text,
+                completed: false,
+            },
+        ];
+    default:
+        return state;
+    }
+};
+const testAddTodo = () => {
+    const stateBefore = [];
+    const action = {
+        type: 'ADD_TODO',
+        id: 0,
+        text: 'learn Redux',
+    };
+    const stateAfter = [
+        {
+            id: 0,
+            text: 'learn Redux',
+            completed: false,
+        },
+    ];
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+    expect(todos(stateBefore, action)).toEqual(stateAfter);
+};
+
+testAddTodo();
+console.log('All tests padded.');
