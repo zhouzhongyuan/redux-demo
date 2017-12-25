7651be9267f9c94614d1cb35ae75b1179499e982
Zhongyuan <yawenzhongyuan@gmail.com>
2017-12-25 16:03:02 +0800

    02-12, Add function addLoggingToDispatch

--- 02/todo/src/configStore.js
+++ 02/todo/src/configStore.js
@@ -3,12 +3,30 @@ import throttle from 'lodash/throttle';
 import todoApp from './reducers';
 import { loadState, saveState } from './localStorage';
 
+const addLoggingToDispatch = (store) => {
+    const rawDidpatch = store.dispatch;
+    if (!console.group) {
+        return rawDidpatch;
+    }
+    return (action) => {
+        console.group(action.type);
+        console.log('%c prev state', 'color: gray', store.getState());
+        console.log('%c action', 'color: blue', action);
+        const returnValue = rawDidpatch(action);
+        console.log('%c next state', 'color: green', store.getState());
+        console.groupEnd(action.type);
+        return returnValue;
+    };
+};
 const configStore = () => {
     const persistedState = loadState();
     const store = createStore(
         todoApp,
         persistedState,
     );
+    if (process.env.NODE_ENV !== 'production') {
+        store.dispatch = addLoggingToDispatch(store);
+    }
     store.subscribe(throttle(() => {
         saveState({
             todos: store.getState().todos,
