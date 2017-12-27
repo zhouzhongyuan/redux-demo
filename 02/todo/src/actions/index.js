1429fd61f9214c3d3085e0b85566c00eaf160f56
Zhongyuan <yawenzhongyuan@gmail.com>
2017-12-27 16:46:45 +0800

    02-25, Creating Data on the Server

--- 02/todo/src/actions/index.js
+++ 02/todo/src/actions/index.js
@@ -1,12 +1,14 @@
-import { v4 } from 'node-uuid';
 import { getIsFetching } from '../reducers';
 import * as api from '../api';
 
-export const addTodo = text => ({
-    type: 'ADD_TODO',
-    id: v4(),
-    text,
-});
+export const addTodo = text => (dispatch) => {
+    api.addTodo(text).then((response) => {
+        dispatch({
+            type: 'ADD_TODO_SUCCESS',
+            response,
+        });
+    });
+};
 
 export const toggleTodo = id => ({
     type: 'TOGGLE_TODO',
--- 02/todo/src/api/index.js
+++ 02/todo/src/api/index.js
@@ -24,9 +24,6 @@ const delay = ms =>
 // eslint-disable-next-line
 export const fetchTodos = filter =>
     delay(500).then(() => {
-        if (Math.random() > 0.5) {
-            throw new Error('Boom!');
-        }
         switch (filter) {
         case 'all':
             return fakeDatabase.todos;
@@ -37,4 +34,22 @@ export const fetchTodos = filter =>
         default:
             return new Error(`Unknown filter: ${filter}`);
         }
+    });
+
+export const addTodo = text =>
+    delay(500).then(() => {
+        const todo = {
+            id: v4(),
+            text,
+            completed: false,
+        };
+        fakeDatabase.todos.push(todo);
+        return todo;
+    });
+
+export const toggleTodo = id =>
+    delay(500).then(() => {
+        const todo = fakeDatabase.todos.find(t => t.id === id);
+        todo.completed = !todo.completed;
+        return todo;
     });
