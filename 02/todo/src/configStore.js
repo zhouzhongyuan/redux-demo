ca6adfcd7c63d54273d5058868f039795ee8b389
Zhongyuan <yawenzhongyuan@gmail.com>
2017-12-27 11:31:44 +0800

    02-18, Applying Redux Middleware.import redux-promise and redux-logger

--- 02/todo/src/configStore.js
+++ 02/todo/src/configStore.js
@@ -1,38 +1,18 @@
 import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
+import createLogger from 'redux-logger';
 import todoApp from './reducers';
 
-const logger = store => (next) => {
-    if (!console.group) {
-        return next;
-    }
-    return (action) => {
-        console.group(action.type);
-        console.log('%c prev state', 'color: gray', store.getState());
-        console.log('%c action', 'color: blue', action);
-        const returnValue = next(action);
-        console.log('%c next state', 'color: green', store.getState());
-        console.groupEnd(action.type);
-        return returnValue;
-    };
-};
-// eslint-disable-next-line no-unused-vars
-const promise = store => next => (action) => {
-    if (typeof action.then === 'function') {
-        return action.then(next);
-    }
-    return next(action);
-};
 const configStore = () => {
     const middlewares = [promise];
     if (process.env.NODE_ENV !== 'production') {
-        middlewares.push(logger);
+        middlewares.push(createLogger());
     }
 
-    const store = createStore(
+    return createStore(
         todoApp,
         applyMiddleware(...middlewares),
     );
-    return store;
 };
 
 export default configStore;
